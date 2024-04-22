4dbd34e9b6c408d43e56ca69bdd2ef0613fffcf0
DorinVieru <dorino.vieru@gmail.com>
2024-04-22 16:32:50 +0200

    up: Creation class for Biblioteca with function to add utente, documento e prestito

--- csharp-biblioteca/Biblioteca.cs
+++ csharp-biblioteca/Biblioteca.cs
@@ -8,6 +8,35 @@ namespace csharp_biblioteca
 {
     internal class Biblioteca
     {
-       
+        public List<Utente> Utenti { get; set; }
+        public List<Documenti> Documenti { get; set; }
+        public List<Prestito> Prestiti { get; set; }
+
+        public Biblioteca()
+        {
+            Utenti = new List<Utente>();
+            Documenti = new List<Documenti>();
+            Prestiti = new List<Prestito>();
+        }
+
+        // FUNZIONE PER AGGIUNGERE UN UTENTE
+        public void AggiungiUtente(Utente utente)
+        {
+            Utenti.Add(utente);
+        }
+
+        // FUNZIONE PER AGGIUNGERE UN DOCUMENTO
+        public void AggiungiDocumento(Documenti documento)
+        {
+            Documenti.Add(documento);
+        }
+
+        // FUNZIONE PER AGGIUNGERE UN PRESTITO
+        public void AggiungiPrestito(Prestito prestito)
+        {
+            Prestiti.Add(prestito);
+        }
+
+        
     }
 }
