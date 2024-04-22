e15180b53b4ddeaacc6fa4dee032950168e31328
DorinVieru <dorino.vieru@gmail.com>
2024-04-22 16:57:01 +0200

    UP: Creation a new book & DVD

--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -21,6 +21,15 @@ namespace csharp_biblioteca
             biblioteca.AggiungiUtente(utente);
             Console.WriteLine($"Nuovo utente registrato: {utente.Nome} {utente.Cognome}, {utente.RecapitoTelefonico}");
 
+            // Creazione di un libro
+            Libro libro = new Libro("LB346", "Io Sono il Padrone della mia Anima", 2018, "Crescita personale", "S1", "Salvatore Brizzi", 222);
+            biblioteca.AggiungiDocumento(libro);
+            Console.WriteLine($"Nuovo libro creato: {libro.Codice} {libro.Titolo}, {libro.Anno}, genere {libro.Settore}, di {libro.Autore}, {libro.NumeroPagine} pagine.");
+
+            // Creazione di un DVD
+            DVD dvd = new DVD("DVD077", "Matrix", 1999, "Sci-Fi (Realistico)", "B2", "Lana % Lily Wachowski", 136);
+            biblioteca.AggiungiDocumento(dvd);
+            Console.WriteLine($"Nuovo DVD creato: {dvd.Codice} {dvd.Titolo}, {dvd.Anno}, genere {dvd.Settore}, di {dvd.Autore}, {dvd.Durata} minuti.");
 
             
         }
