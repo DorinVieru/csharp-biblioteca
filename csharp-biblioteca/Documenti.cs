7b6d8ca9dc22bf5c3c7dc847edc141f85f4966f9
DorinVieru <dorino.vieru@gmail.com>
2024-04-22 16:02:36 +0200

    UP: Creation class of Documenti

--- csharp-biblioteca/Documenti.cs
+++ csharp-biblioteca/Documenti.cs
@@ -8,7 +8,34 @@ namespace csharp_biblioteca
 {
     internal class Documenti
     {
-        
+        public string Codice { get; set; }
+        public string Titolo { get; set; }
+        public int Anno { get; set; }
+        public string Settore { get; set; }
+        public string Scaffale { get; set; }
+        public string Autore { get; set; }
+
+        public Documenti(string codice, string titolo, int anno, string settore, string scaffale, string autore)
+        {
+            Codice = codice;
+            Titolo = titolo;
+            Anno = anno;
+            Settore = settore;
+            Scaffale = scaffale;
+            Autore = autore;
+        }
+
+        public virtual void Prestito()
+        {
+            Console.WriteLine($"'{Titolo}' è stato preso in prestito.");
+        }
+
+        public virtual void Ritorno()
+        {
+            Console.WriteLine($"'{Titolo}' è stato restituito.");
+        }
+
+       
     }
 
 }
