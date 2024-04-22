89a50704c9d3484fc74adb9d88de0b94b584784c
DorinVieru <dorino.vieru@gmail.com>
2024-04-22 16:54:51 +0200

    UP: Creation a new user

--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -6,6 +6,22 @@ namespace csharp_biblioteca
     {
         static void Main(string[] args)
         {
+            // Creazione di un'istanza della biblioteca
+            Biblioteca biblioteca = new Biblioteca();
+
+            // Creazione di un utente
+            Utente utente = new Utente
+            {
+                Cognome = "Dorin",
+                Nome = "Bot",
+                Email = "bot.dorin@email.com",
+                Password = "Lamiabellapassw",
+                RecapitoTelefonico = "34967589867"
+            };
+            biblioteca.AggiungiUtente(utente);
+            Console.WriteLine($"Nuovo utente registrato: {utente.Nome} {utente.Cognome}, {utente.RecapitoTelefonico}");
+
+
             
         }
     }
